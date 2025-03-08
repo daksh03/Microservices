3b60e347832a8a44382f0951ae5251e10c9f91c7
daksh03 <dakshchoudhary.1234@gmail.com>
2025-03-08 13:46:18 +0530

    I18n implementation

--- src/main/java/com/microservices/practice/microservices_project/controller/HelloController.java
+++ src/main/java/com/microservices/practice/microservices_project/controller/HelloController.java
@@ -1,5 +1,9 @@
 package com.microservices.practice.microservices_project.controller;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +13,21 @@ import com.microservices.practice.microservices_project.bean.HelloBean;
 @RestController
 public class HelloController {
 	
+	private MessageSource messageSource;
+	
+	public HelloController(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
+	
+	
 	@GetMapping("/hello")
 	public String helloWorld() {
 		return "Hello";
+	}
+	@GetMapping("/hello-inter")
+	public String helloWorldinternational() {
+		Locale locale = LocaleContextHolder.getLocale();
+		return messageSource.getMessage("good.morning.message", null,"Default Message", locale);
 	}
 	
 	@GetMapping("/hello-bean")
